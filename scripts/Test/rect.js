598b836dce4985f3eca42007aca124b45967137d
hpanago <hpanago@gmail.com>
2014-04-26 23:34:06 +0300

    added checkpoints

--- scripts/Test/rect.js
+++ scripts/Test/rect.js
@@ -33,6 +33,17 @@ factory['Rect'] = Class.extend({
 	life: 100,
 	maxLife: 100,
 
+	move: false,
+
+	checkpoints: [],
+	
+	pointCounter: 0,
+
+	newPos: {
+		x: 0,
+		y: 0
+	},
+
 	go: {
 		'up':   0,
 		'down': 0,
@@ -52,6 +63,53 @@ factory['Rect'] = Class.extend({
 
                 this.pos.x += this.speed.x;
                 this.pos.y += this.speed.y;
+		
+		if ( this.move == true ) {
+			//this.speed.x = 1;
+		
+			if ( this.pos.x < this.newPos.x){
+				this.speed.x = 1;
+			}
+			else if ( this.pos.x > this.newPos.x) {
+				this.speed.x = -1;
+			} 
+			
+			if ( this.pos.y < this.newPos.y) {
+				this.speed.y = 1;
+			}
+			else if ( this.pos.y > this.newPos.y) {
+				this.speed.y = -1;
+			}
+
+
+
+		}
+		
+	
+			
+
+		if ( this.pos.x == this.newPos.x &&  this.pos.y ==  this.newPos.y) {
+			this.speed.x = 0;
+			this.speed.y = 0;
+			this.move = false;
+		}
+		
+		if (this.checkpoints.length > 0 && this.move == false ) {
+			var newLocation = this.checkpoints[this.pointCounter];
+ 			this.moveTo(newLocation.x, newLocation.y)
+			this.pointCounter += 1;
+	
+		}
+		
+	
+		if ( this.pointCounter >= this.checkpoints.length ) {
+			this.pointCounter = 0;
+		}
+		
+
+		
+	
+
 	},
 	
 
@@ -62,6 +120,22 @@ factory['Rect'] = Class.extend({
 	draw: function (ctx) {
       	
 		Drawer.rect(this.pos.x,this.pos.y,this.width,this.height); 
+	},
+
+
+
+
+	moveTo: function (x,y) {
+		this.newPos.x = x;
+		this.newPos.y = y;
+		if ( this.pos.x != this.newPos.x && this.pos.y != this.newPos.y ) {
+			this.move = true; 
+		}
+	},	    
+
+
+	addCheckpoint: function (x,y) {
+		this.checkpoints.push({x:x,y:y})
 	}
-	    
+
 })
