81df0c5f5789e8691e5932b5c85c2ea5a550d795
hpanago <hpanago@gmail.com>
2014-04-26 19:27:06 +0300

    add test rect

---
+++ scripts/Test/rect.js
@@ -0,0 +1,53 @@
+factory['Rect'] = Class.extend({ 
+
+	_killed: false,
+	crashed: false,	
+
+	surface: null,
+    color: null,   
+
+	zIndex: 3,	
+
+	width: 20,
+	height: 20,
+
+	pos: {
+		x: 150,
+		y: 250
+	},
+
+	speed: { 
+		x:0,
+		y:0
+	},
+
+	ang: 0,
+
+    nextSurface: false,
+
+	maxSpeed: 2,
+
+	img: null,
+	imgSrc: assets['Prisma'],
+
+	life: 100,
+	maxLife: 100,
+
+	go: {
+		'up':   0,
+		'down': 0,
+		'front':0	
+	},
+
+	init: function(x, y) {
+
+		x = this.pos.x;
+		y = this.pos.y;
+		this.img = Loader.load( this.imgSrc);
+
+	},
+draw: function (ctx) {
+      	
+    Drawer.rect(x,y) } 
+	    
+})
