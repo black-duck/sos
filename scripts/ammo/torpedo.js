04f3b28ebd9f1a227f185cca51d26101a7cb1cbe
Giorgos Anagnostopoulos <ganag16@gmail.com>
2014-04-26 20:33:28 +0300

    Torpedo object !

---
+++ scripts/ammo/torpedo.js
@@ -0,0 +1,106 @@
+
+factory['torpedo'] = Class.extend({ 
+
+	_killed: false,
+
+	physBody: null,
+	
+	speed: 700,
+	lifetime: 5000,
+	damage: 1,
+
+	size: {
+		x: 5,
+		y: 10
+	},
+	
+	pos: {
+		x: 0,
+		y: 0
+	},
+
+	dir: {
+		x: 0,
+		y: 0
+	},
+
+
+	img: assets['torpedo'],
+
+	init: function(sX, sY, eX, eY) {
+
+		this.pos.x = sX;
+		this.pos.y = sY;
+		this.dir.x = eX;
+		this.dir.y = eY;
+
+		this.physBody = PhysicsEngine.addBody({
+							
+								id: 'torpedo',
+	                             x: sX,
+	                        	 y: sY,
+								 damping: 0,
+								 bullet: true,
+								 density: 0,
+	                        	 userData: { id: 'torpedo',
+	                            	         ent: this 
+	                                     },
+	            				 //angle: Geometry.vecToRad(this.dir.x, this.dir.y), 
+	                             halfWidth: this.size.y/2,
+	                             halfHeight: this.size.x/2,
+								
+	                        });     
+		
+		var vec = new Vec2(this.dir.x, this.dir.y);
+		vec.Normalize();
+		vec.Multiply(this.speed);
+		this.physBody.SetLinearVelocity(vec);
+
+        if (arguments.length == 5) {
+            if (arguments[4] == "red") {
+                this.img = assets['torpedo'];
+            }
+        }
+	
+	},
+
+	update: function() {
+		 
+		var vec = new Vec2(this.dir.x, this.dir.y);
+		vec.Normalize();
+		vec.Multiply(this.speed);
+		this.physBody.SetLinearVelocity(vec);
+		
+		if (this.physBody != null) {
+			var pPos = this.physBody.GetPosition();
+			this.pos.x = pPos.x;
+			this.pos.y = pPos.y;
+		}
+		if (this.lifetime <= 0) {
+			this.kill();
+		}
+		else {
+			this.lifetime -= 1000/60;
+		}
+
+	},
+
+	kill: function () {
+		this._killed= true;
+	},
+
+	draw: function(ctx) {
+		var rad = this.physBody.GetAngle();	
+		Drawer.image( this.img, 
+						this.pos.x, this.pos.y, rad, 
+						this.size.x, this.size.y );
+
+	},
+
+	onImpact: function(otherEnt) {
+		otherEnt.damage(this.damage);
+		this.kill();
+	}
+
+	
+});
