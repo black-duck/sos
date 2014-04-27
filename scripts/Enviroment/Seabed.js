9ef66e391d9727a2c0a9982f28e273916ae9a332
lvrach <leo.al.vra@gmail.com>
2014-04-27 15:29:14 +0300

    improve Seabed

--- scripts/Enviroment/Seabed.js
+++ scripts/Enviroment/Seabed.js
@@ -9,50 +9,73 @@ Seabed = {
 	},
 
 
-	width: 100,
+	width: 800,
 	height: 8,
+
+	step: 20,
+	speed: 1,
+	wait: 0,
 
 	depths: [],
 
-	color: "black",
+	color: "grey",
+	bgcolor: "black",
+
+	__ddelay: 0,
 
 	init: function () {
 	
-		SoundGen.fft = function (array) {
-
-			var sum = 0;
-			for (var i=10; i < 300; i++) {
-				sum += array[i]
-			}
-
-			
-			Seabed.depths.unshift(sum/50);
-			if (Seabed.depths.length > 300) {
-				Seabed.depths.pop()
-			}
-
-		}
 		
 	},
 
 	update: function () {
 	
-		
+		if ( this.wait > this.step ) {
+			var array = new Uint8Array(1024)
+			SoundGen.analyser.getByteFrequencyData(array)
 
+			var sum = 0, i;
+			for (i=0; i < 300 && i < array.length; i++) {
+				sum += array[i]
+			}
+
+			Seabed.depths.unshift(sum/i);
+			this.__ddelay = (this.__ddelay + 1) % 2
+
+			this.wait = 0
+		}
+		else {
+			this.wait += this.speed
+		}
 	},
 
 
 	draw: function () {
 		
-		var i = 0;
-		for ( d in this.depths ) {
+		for ( var i = 0, x = this.pos.x + this.width + 2;
+				i < this.depths.length && x > this.pos.x; 
+				i++, x -= this.step ) {
 
-			Drawer.rect(this.pos.x + i*10, this.pos.y - this.depths[d]/2,  
-					 	 10, this.depths[d], 
+			d = this.depths[i] 
+				
+			Drawer.rect(x, this.pos.y - d/2,  
+					 	 this.step, d, 
+						 this.bgcolor, this.bgcolor )
+		}	
+
+	
+	
+		for ( var i = 0, x = this.pos.x + this.width;
+				i < this.depths.length && x > this.pos.x; 
+				i++, x -= this.step ) {
+
+			d = this.depths[i] 
+			d = 2*d/3  
+				
+			Drawer.rect(x, this.pos.y - d/2,  
+					 	 this.step, d, 
 						 this.color, this.color)
-			i++
 		}
-		
 
 	}
 
